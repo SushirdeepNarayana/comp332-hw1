fe8e2098e38b44b1605c0662d470cec92ed7a551
SushirdeepNarayana <snarayana@wesleyan.edu>
2025-02-04 22:02:04 -0500

    initial hw1 commit

---
+++ hw1.py
@@ -0,0 +1,38 @@
+'''
+    You will only be writing code in this file .
+'''
+
+def get_version():
+    '''
+        print the version of python that you are using. Use the sys library module.
+    '''
+    "*** WRITE YOUR CODE HERE ***"
+
+
+def longest_palindrome(s):
+    '''
+         Given a string s, return the longest palindromic substring in s 
+    '''
+    "*** WRITE YOUR CODE HERE ***"
+
+
+def counting_bits(n):
+    '''
+         Given an integer n, return a list say 'ans' of length n+1 such that for each i (0 <= i <=n), ans[i] is the number of 1's in the binary representation of i 
+    '''
+    "*** WRITE YOUR CODE HERE ***"
+ 
+
+def reverse_bits(n):
+    '''
+         Reverse bits of a given 32 bits unsigned integer.
+    '''
+    "*** WRITE YOUR CODE HERE ***"
+ 
+
+def order_scores():
+    '''
+        read the files scores.csv in the data directory and return the list of names sorted in ascending order of the scores.
+    '''
+    "*** WRITE YOUR CODE HERE ***"
+
